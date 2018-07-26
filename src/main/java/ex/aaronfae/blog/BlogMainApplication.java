4f6848dc6fc722d1470e30e18d1d03f066108689
AaronFae <AaronFae@163.com>
2018-07-26 16:33:25 +0800

    init project

---
+++ src/main/java/ex/aaronfae/blog/BlogMainApplication.java
@@ -0,0 +1,12 @@
+package ex.aaronfae.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogMainApplication.class, args);
+	}
+}
